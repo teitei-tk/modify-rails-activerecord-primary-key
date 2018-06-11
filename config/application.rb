f7d30da45f2fab369d4833038b2b8f9ed47e8aa1
teitei-tk <teitei.tk@gmail.com>
2018-06-11 23:02:32 +0900

    modify default primary Keys from BIGINT to INTEGER

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,9 @@ module ModifyRailsActiverecordPrimaryKey
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.generators do |generator|
+      generator.orm :active_record, primary_key_type: :integer
+    end
   end
 end
